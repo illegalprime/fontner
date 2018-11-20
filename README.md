9783ad95ed5efd834a890c7f89a266b6d6567545
Michael Eden <themichaeleden@gmail.com>
2018-11-20 01:06:25 -0500

    add option to specify font files

--- README.md
+++ README.md
@@ -7,6 +7,20 @@ For the glyph `0xFF03` run:
 ./find_glyph.py ff03
 ```
 
+Or specify your own files!
+
+```
+./find_glyph.py ff03 <ttf path 1> <ttf path 2> ...
+```
+
+You should see this (meaning `unifont` supports this glyph):
+
+```
+$ ./find_glyph.py ff03 
+FULLWIDTH NUMBER SIGN
+/nix/store/adznf6h69af320wpmlhx525l6hfizaqw-unifont-11.0.02/share/fonts/truetype/unifont.ttf
+```
+
 # Install
 
 To install python & needed packages just use a [nix](https://nixos.org/nix/) shell:
--- find_glyph.py
+++ find_glyph.py
@@ -9,21 +9,19 @@ from fontTools.unicode import Unicode
 glyph = int(sys.argv[1], 16)
 print(Unicode[glyph])
 
-fc_list = subprocess.check_output(["fc-list"]).decode()
+if len(sys.argv) > 2:
+    paths = sys.argv[2:]
+else:
+    lines = subprocess.check_output(["fc-list"]).decode().split()
+    ttfs = filter(lambda l: l[-4:] == "ttf:", lines)
+    paths = map(lambda t: t[:-1], ttfs)
 
-for line in fc_list.split():
-    if line[-4:] != "ttf:":
-        continue
-    path = line[:-1]
-
-    ttf = TTFont(path, 0, verbose=0, allowVID=0, ignoreDecompileErrors=True, fontNumber=-1)
-
-    chars = chain.from_iterable([y + (Unicode[y[0]],) for y in x.cmap.items()] for x in ttf["cmap"].tables)
-
+for path in paths:
+    ttf = TTFont(path, 0, allowVID=0, ignoreDecompileErrors=True, fontNumber=-1)
+    chars = chain.from_iterable([y + (Unicode[y[0]],) for y in x.cmap.items()]
+                                for x in ttf["cmap"].tables)
     exists = glyph in (x[0] for x in chars)
-
     ttf.close()
-
     if exists:
         print(path)
 
